eed0a13e078c0ef5b66d8d98c6ee5099ca298cfd
prathap <prathapkunarapu1@gmail.com>
2022-05-08 22:28:19 +0530

    first commit

---
+++ firstprogram3.java
@@ -0,0 +1,22 @@
+class firstprogram3 {
+    public static void main(String [] args){
+    
+
+        // sum of two integers 
+        int a = 10;
+        int d = 2;
+        int sum = a+d;
+        System.out.println(sum);
+       // area of rectangle
+       int l = 20;
+       int b = 10;
+       int area = l*b;
+       System.out.println(area);
+
+
+
+
+
+
+    }
+}
